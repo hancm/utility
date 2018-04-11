51d798c746acb7e26bc5a6818d6ad38c7bdcc4af
hanchangming <hanchangming@ancun.com>
2018-04-11 16:13:12 +0800

    cuda辅助函数修改

--- helper_cuda.h
+++ helper_cuda.h
@@ -994,33 +994,23 @@ static const char *_cudaGetErrorEnum(NppStatus error)
 #endif
 #endif
 
-//template< typename T >
-//void check(T result, char const *const func, const char *const file, int const line)
-//{
-//    if (result)
-//    {
-//        fprintf(stderr, "CUDA error at %s:%d code=%d(%s) \"%s\" \n",
-//                file, line, static_cast<unsigned int>(result), _cudaGetErrorEnum(result), func);
-//        DEVICE_RESET
-//        // Make sure we call CUDA Device Reset before exiting
-//        exit(EXIT_FAILURE);
-//    }
-//}
+template< typename T >
+void check(T result)
+{
+    if (result)
+    {
+        LOG_ERROR("CUDA runtime error code={}({}):{}.", result, _cudaGetErrorEnum(result), cudaGetErrorString(result));
+        DEVICE_RESET
+        // Make sure we call CUDA Device Reset before exiting
+        exit(EXIT_FAILURE);
+    }
+}
 
 #ifdef __DRIVER_TYPES_H__
 // This will output the proper CUDA error strings in the event that a CUDA host call returns an error
-//#define checkCudaErrors(val)           check ( (val), #val, __FILE__, __LINE__ )
-
-#define checkCudaErrors( a ) do { \
-    cudaError_t err = (a); \
-    if (cudaSuccess != err) { \
-        LOG_ERROR("Cuda runtime error code={}({}):{}.", err, _cudaGetErrorEnum(err), cudaGetErrorString(err)); \
-        exit(EXIT_FAILURE); \
-    } \
-} while(0);
+#define checkCudaErrors(val)           check ( (val) )
 
 // This will output the proper error string when calling cudaGetLastError
-
 #define getLastCudaError(msg)      __getLastCudaError (msg)
 
 inline void __getLastCudaError(const char *errorMessage)
@@ -1033,21 +1023,6 @@ inline void __getLastCudaError(const char *errorMessage)
         exit(EXIT_FAILURE);
     }
 }
-
-//#define getLastCudaError(msg)      __getLastCudaError (msg, __FILE__, __LINE__)
-//
-//inline void __getLastCudaError(const char *errorMessage, const char *file, const int line)
-//{
-//    cudaError_t err = cudaGetLastError();
-//
-//    if (cudaSuccess != err)
-//    {
-//        fprintf(stderr, "%s(%i) : getLastCudaError() CUDA error : %s : (%d) %s.\n",
-//                file, line, errorMessage, (int)err, cudaGetErrorString(err));
-//        DEVICE_RESET
-//        exit(EXIT_FAILURE);
-//    }
-//}
 
 // This will only print the proper error string when calling cudaGetLastError but not exit program incase error detected.
 #define printLastCudaError(msg)      __printLastCudaError (msg, __FILE__, __LINE__)
