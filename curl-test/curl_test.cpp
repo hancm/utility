0c96e757aa4f12b68dde2afe3d93c9901cfcaeaf
hancm <hancm.hi@163.com>
2018-12-20 15:45:18 +0800

    curl

---
+++ curl-test/curl_test.cpp
@@ -0,0 +1,116 @@
+#include "curl/curl.h"
+#include <stdio.h>
+#include <iostream>
+
+#define IN
+#define INOUT
+#define OUT
+
+using std::cout;
+using std::endl;
+
+static std::string g_strRecvbuf;
+
+/**
+ * @brief 接收数据的回调函数
+ * @param [IN] pRecv        接收数据的缓冲区
+ * @param [IN] iBlockNum    接收的数据块的数目
+ * @param [IN] iBlockSize   接收的数据块的大小
+  * @param [IN] pUserData   用户参数
+ * @return - size_t
+ * -
+ * @author h02199
+ * @time   2016/1/28
+ * @attention 可能分多次接收完所有的数据
+ */
+static size_t WriteCallback(IN char *pRecv, IN size_t iBlockNum, IN size_t iBlockSize, IN void *pUserData)
+{
+    /* 用户参数 */
+    char *pData = (char*)pUserData;
+    cout << pData << endl;
+
+    /* 数据大小 */
+    size_t iDataLen = iBlockNum * iBlockSize;
+
+    cout << iBlockNum << endl << iBlockSize << endl;
+
+    g_strRecvbuf.append(pRecv, iDataLen);
+
+    return iBlockNum * iBlockSize;
+}
+
+int main(void)
+{
+    /**
+     * 设置全局libcurl环境信息
+     * 进程有且只能调用一次，非线程安全的。
+     * 需要在主线程完成初始化, 若是没有被调用，则在curl_easy_init会设置一个默认模式，
+     * 这样可能会导致多次调用发生错误。
+     * CURL_GLOBAL_ALL: 一般是好的设置方式，初始化SSL、WIN32
+     */
+    (void)curl_global_init(CURL_GLOBAL_ALL);
+
+    /**
+     * libcurl的同步阻塞方式
+     * 返回的easy handle只能在同一个线程中被调用，不能在多个线程使用。
+     * 多个线程时，每个线程都必须创建自己的handle。
+     */
+    CURL *pCurlHandle = curl_easy_init();
+    if (NULL == pCurlHandle)
+    {
+        cout << "Failed to curl_easy_init" << endl;
+        return -1;
+    }
+	
+    /**
+     * libcurl设置合适的操作选项
+     * 所有的选项都用同一个handle,根据不同的选项设置不同的参数。
+     * 函数内部会保留参数的副本，参数可以随后释放。
+     */
+    /* CURLOPT_URL: 设置需要的URL */
+    (void)curl_easy_setopt(pCurlHandle, CURLOPT_URL, "www.baidu.com");
+
+    /* 告诉libcurl重定向了URL：1L打开 */ 
+    (void)curl_easy_setopt(pCurlHandle, CURLOPT_FOLLOWLOCATION, 1L);
+
+    /**
+     * 设置回调函数
+     * 没有设置则默认输出到stdout
+     * 需要接收的数据可能分多次接收完成
+     */
+    (void)curl_easy_setopt(pCurlHandle, CURLOPT_WRITEFUNCTION, WriteCallback);
+
+    /**
+     * 给回调函数传递用户参数
+     * 可以通过修改参数向不同的FILE*打印数据
+     */
+    (void)curl_easy_setopt(pCurlHandle, CURLOPT_WRITEDATA, "hancm");
+
+    /**
+     * 调试用：输出所有可能的错误信息
+     * 正式产品中不应该使用
+     */
+    (void)curl_easy_setopt(pCurlHandle, CURLOPT_VERBOSE, 1L);
+
+    /**
+     * 连接到远程主机，执行相应的命令，等待返回结果。
+     * 会调用先前设置的回调函数接受数据，若没有设置则输出到标准输出。
+     * 一次可能只接收一个字节或者很多字节，libcurl会尽可能多的分发数据。
+     */
+    CURLcode curlRet = curl_easy_perform(pCurlHandle);
+    if(curlRet != CURLE_OK)
+    {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(curlRet));
+    }
+
+    cout << "#########: " << endl
+         << g_strRecvbuf.c_str() << endl;
+
+    /* 清理easy handle */ 
+    curl_easy_cleanup(pCurlHandle);
+
+    /* 清理全局libcurl环境信息 */
+    curl_global_cleanup();
+
+    return 0;
+}
