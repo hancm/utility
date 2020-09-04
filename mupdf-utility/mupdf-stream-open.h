55e405f6633fe0abd5a9291e700129366f53fed5
hanchangming <hanchangming@ancun.com>
2020-09-04 15:48:57 +0800

    mupdf 删除

--- mupdf-utility/mupdf-stream-open.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MUPDF_STREAM__OPEN__H__
-#define __MUPDF_STREAM__OPEN__H__
-#include "mupdf/fitz.h"
-
-fz_output *fz_new_output_with_stream(fz_context *ctx, std::iostream *stream);
-
-#endif /* __MUPDF_STREAM__OPEN__H__ */
